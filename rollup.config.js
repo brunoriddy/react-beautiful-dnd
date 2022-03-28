a05646959c19f4ef4bbdac5a578c5eea752d3feb
Bruno Riddy <bruno.riddy@telegraph.co.uk>
2022-03-28 15:56:16 +0100

    built

--- rollup.config.js
+++ rollup.config.js
@@ -31,18 +31,25 @@ const snapshotArgs =
       }
     : {};
 
-const commonjsArgs = {
-  include: 'node_modules/**',
-  // needed for react-is via react-redux
-  // https://stackoverflow.com/questions/50080893/rollup-error-isvalidelementtype-is-not-exported-by-node-modules-react-is-inde/50098540
-  namedExports: {
-    'node_modules/react-is/index.js': [
-      'isValidElementType',
-      'isContextConsumer',
-    ],
-  },
-};
-
+    const commonjsArgs = {
+      include: 'node_modules/**',
+      // needed for react-is via react-redux
+      // https://stackoverflow.com/questions/50080893/rollup-error-isvalidelementtype-is-not-exported-by-node-modules-react-is-inde/50098540
+      namedExports: {
+        'node_modules/react-redux/node_modules/react-is/index.js': [
+          'isValidElementType',
+          'isContextConsumer',
+        ],
+        'node_modules/react-is/index.js': [
+          'isValidElementType',
+          'isContextConsumer',
+        ],
+        'node_modules/react-redux/es/components/connectAdvanced.js': [
+          'isContextConsumer',
+        ],
+      },
+    };
+    
 export default [
   // Universal module definition (UMD) build
   // - including console.* statements
